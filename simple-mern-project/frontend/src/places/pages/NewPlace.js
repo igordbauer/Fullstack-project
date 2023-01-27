971450656854bf5525337bd8196952db53e29276
igordbauer <igordbauer@gmail.com>
2023-01-27 10:28:57 -0300

    setup-first-routes

---
+++ simple-mern-project/frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const NewPlace = () => {
+  return <h1>NewPlace</h1>;
+};
+export default NewPlace;
---
+++ simple-mern-project/frontend/src/user/pages/Users.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Users = () => {
+  return <h1>hi</h1>;
+};
+export default Users;
