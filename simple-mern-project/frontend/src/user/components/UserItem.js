312696fc18f675405b9c2c3a8fea3f177de716e4
igordbauer <igordbauer@gmail.com>
2023-01-28 10:37:09 -0300

    avatar-component

--- simple-mern-project/frontend/src/user/components/UserItem.js
+++ simple-mern-project/frontend/src/user/components/UserItem.js
@@ -1,6 +1,21 @@
 import "./UserItem.css";
+import React from "react";
 
-const UserItem = () => {
-  return;
+const UserItem = ({ image, name, placeCount }) => {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={image} alt={name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{name}</h2>
+          <h3>
+            {placeCount} {placeCount === 1 ? "Place" : "Places"}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
 };
 export default UserItem;
--- simple-mern-project/frontend/src/user/components/UsersList.js
+++ simple-mern-project/frontend/src/user/components/UsersList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./UserList.css";
+import "./UsersList.css";
 import UserItem from "./UserItem";
 
 const UserList = ({ items }) => {
