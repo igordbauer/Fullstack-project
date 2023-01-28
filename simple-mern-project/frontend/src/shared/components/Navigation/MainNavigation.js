0cf9ccf6799e9924cb8470d1b9f4e3cb124a7400
igordbauer <igordbauer@gmail.com>
2023-01-28 11:40:12 -0300

    implementing-navlinks

--- simple-mern-project/frontend/src/shared/components/Navigation/MainNavigation.js
+++ simple-mern-project/frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import "./MainNavigation.css";
 
 const MainNavigation = () => {
   return (
@@ -14,7 +15,9 @@ const MainNavigation = () => {
       <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
