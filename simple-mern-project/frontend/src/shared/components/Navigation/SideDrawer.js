c87fc46ba07df36b0ec09719880ba8f023d53980
igordbauer <igordbauer@gmail.com>
2023-01-28 12:14:30 -0300

    working-in-drawer-animation

--- simple-mern-project/frontend/src/shared/components/Navigation/SideDrawer.js
+++ simple-mern-project/frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
 import "./SideDrawer.css";
 
-const SideDrawer = ({ children }) => {
-  const content = <aside className="side-drawer">{children}</aside>;
+const SideDrawer = ({ children, show }) => {
+  const content = (
+    <CSSTransition in={show} timeout={200}>
+      <aside className="side-drawer">{children}</aside>;
+    </CSSTransition>
+  );
 
   return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
 };
