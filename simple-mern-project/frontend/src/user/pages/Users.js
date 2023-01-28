f917e66c1295d4ba6d65ba67d9668d3f97fc5bdb
igordbauer <igordbauer@gmail.com>
2023-01-28 09:46:01 -0300

    create-UserItem-component

--- simple-mern-project/frontend/src/user/pages/Users.js
+++ simple-mern-project/frontend/src/user/pages/Users.js
@@ -1,5 +1,18 @@
 import React from "react";
+import UserList from "../components/UsersList";
+
 const Users = () => {
-  return <h1>hi</h1>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "igor",
+      image:
+        "http://s2.glbimg.com/h3Duok3KWVA8yaIOzZZIESkNLC4DKPsVVGWWhNMHhpNIoz-HdGixxa_8qOZvMp3w/e.glbimg.com/og/ed/f/original/2013/08/02/imagem_para_sexta_51.jpg",
+      places: 3,
+    },
+  ];
+
+  return <UserList items={USERS} />;
 };
+
 export default Users;
