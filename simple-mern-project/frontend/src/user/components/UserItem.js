e88fea625483a2a8e434d727f64c344e72c57858
igordbauer <igordbauer@gmail.com>
2023-01-28 10:57:59 -0300

    wrap-style-user-component

--- simple-mern-project/frontend/src/user/components/UserItem.js
+++ simple-mern-project/frontend/src/user/components/UserItem.js
@@ -1,20 +1,25 @@
 import "./UserItem.css";
 import React from "react";
+import { Link } from "react-router-dom";
+import Card from "../../shared/components/UIElements/Card";
+import Avatar from "../../shared/components/UIElements/Avatar";
 
-const UserItem = ({ image, name, placeCount }) => {
+const UserItem = ({ id, image, name, placeCount }) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>
-            {placeCount} {placeCount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
--- simple-mern-project/frontend/src/user/components/UsersList.js
+++ simple-mern-project/frontend/src/user/components/UsersList.js
@@ -1,17 +1,21 @@
 import React from "react";
-import "./UsersList.css";
+
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+import "./UsersList.css";
 
 const UserList = ({ items }) => {
   if (items.length === 0) {
     return (
       <div className="center">
-        <h2>No items found.</h2>
+        <Card>
+          <h2>No items found.</h2>
+        </Card>
       </div>
     );
   }
   return (
-    <ul>
+    <ul className="users-list">
       {items.map((user) => (
         <UserItem
           key={user.id}
