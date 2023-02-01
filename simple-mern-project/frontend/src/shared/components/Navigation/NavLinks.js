480eda48169b98872af507505764928657308737
igordbauer <igordbauer@gmail.com>
2023-02-01 09:34:05 -0300

    spreading-auth-links

--- simple-mern-project/frontend/src/shared/components/Navigation/NavLinks.js
+++ simple-mern-project/frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 
 import "./NavLinks.css";
 
 const NavLinks = () => {
+  const authContext = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
@@ -11,15 +14,21 @@ const NavLinks = () => {
           ALL USERS
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">MY PLACES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">ADD PLACE</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>
+      {authContext.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {authContext.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {!authContext.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
