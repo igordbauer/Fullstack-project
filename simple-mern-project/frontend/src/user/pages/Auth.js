8ca02789d45b0d6b0f432cd9c589f6b1ca5e27ab
igordbauer <igordbauer@gmail.com>
2023-02-01 08:55:53 -0300

    add-auth-page

---
+++ simple-mern-project/frontend/src/user/pages/Auth.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormComponents/Button";
+import Input from "../../shared/components/FormComponents/Input";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+
+import "./Auth.css";
+
+const Auth = () => {
+  const [formState, inputHandler] = useForm({
+    email: {
+      value: "",
+      isValid: false,
+    },
+    password: {
+      value: "",
+      isValid: false,
+    },
+  });
+
+  const authSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="E-Mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address"
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid passwrod (at least 5 characters)"
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          Login
+        </Button>
+      </form>
+    </Card>
+  );
+};
+export default Auth;
