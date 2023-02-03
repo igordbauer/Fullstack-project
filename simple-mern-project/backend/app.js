ed280763df881d802473e01018c081c73123dbb3
igordbauer <igordbauer@gmail.com>
2023-02-03 09:11:18 -0300

    Error-handling

--- simple-mern-project/backend/app.js
+++ simple-mern-project/backend/app.js
@@ -7,6 +7,17 @@ const userRoutes = require("./routes/users-routes");
 const app = express();
 
 app.use("/api/places", placesRoutes);
-app.use("/api/user", userRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({
+    message: error.message || "unknown error occurred!",
+  });
+});
+
+// app.use("/api/user", userRoutes);
 
 app.listen(5000);
