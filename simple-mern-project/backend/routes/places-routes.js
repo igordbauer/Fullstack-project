7c31fa1760fdddeb5e4309e1155089dadfec0f78
igordbauer <igordbauer@gmail.com>
2023-02-03 08:31:58 -0300

    place-route

--- simple-mern-project/backend/routes/places-routes.js
+++ simple-mern-project/backend/routes/places-routes.js
@@ -11,13 +11,25 @@ const PLACES = [
       lat: 40.7484474,
       lng: -73.9871516,
     },
+    address: "20 W 34th St, New York 10001",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "Its THE building",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York 10001",
+    creator: "u1",
   },
 ];
 
-router.get("/", (req, res, next) => {
-  console.log("get req in places");
-  res.json({
-    message: "it works",
-  });
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = PLACES.filter((e) => e.id == placeId)[0];
+  res.json({ place });
 });
 module.exports = router;
