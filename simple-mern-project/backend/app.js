d6048b52d1c3da8ce00645178682d5ec0d678442
igordbauer <igordbauer@gmail.com>
2023-02-04 09:56:33 -0300

    creating-post-request-and-postman

--- simple-mern-project/backend/app.js
+++ simple-mern-project/backend/app.js
@@ -6,6 +6,8 @@ const userRoutes = require("./routes/users-routes");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use("/api/places", placesRoutes);
 
 app.use((error, req, res, next) => {
--- simple-mern-project/backend/controllers/places-controller.js
+++ simple-mern-project/backend/controllers/places-controller.js
@@ -1,4 +1,5 @@
 const HttpError = require("../models/http-error");
+const uuid = require("uuid");
 
 const PLACES = [
   {
@@ -21,7 +22,7 @@ const PLACES = [
       lng: -73.9871516,
     },
     address: "20 W 34th St, New York 10001",
-    creator: "u1",
+    creator: "u2",
   },
 ];
 
@@ -37,7 +38,7 @@ const getPlaceById = (req, res, next) => {
 
 const getPlaceByUserId = (req, res, next) => {
   const uid = req.params.uid;
-  const userPlaces = PLACES.find((e) => e.id === uid);
+  const userPlaces = PLACES.find((e) => e.creator === uid);
   if (!userPlaces) {
     return next(
       new HttpError("Could not find a place for the provided user id", 404)
@@ -46,5 +47,21 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ userPlaces });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const createdPlace = {
+    id: uuid.v4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+  PLACES.push(createdPlace);
+  res.status(201).json({ place: createdPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
