b028b58f5803a5a145919311f1a69d6b08a0aa75
igordbauer <igordbauer@gmail.com>
2023-02-04 08:57:38 -0300

    structuring-in-controllers

---
+++ simple-mern-project/backend/controllers/places-controller.js
@@ -0,0 +1,50 @@
+const HttpError = require("../models/http-error");
+
+const PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "Its THE building",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York 10001",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "Its THE building",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York 10001",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = PLACES.find((e) => e.id == placeId);
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id", 404);
+  }
+
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const uid = req.params.uid;
+  const userPlaces = PLACES.find((e) => e.id === uid);
+  if (!userPlaces) {
+    return next(
+      new HttpError("Could not find a place for the provided user id", 404)
+    );
+  }
+  res.json({ userPlaces });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- simple-mern-project/backend/routes/places-routes.js
+++ simple-mern-project/backend/routes/places-routes.js
@@ -1,53 +1,11 @@
 const express = require("express");
 
+const placesControllers = require("../controllers/places-controller");
+
 const router = express.Router();
 
-const PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "Its THE building",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York 10001",
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Empire State Building",
-    description: "Its THE building",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th St, New York 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = PLACES.find((e) => e.id == placeId);
-  if (!place) {
-    const error = new Error("Could not find a place for the provided id");
-    error.code = 404;
-    throw error;
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const uid = req.params.uid;
-  const userPlaces = PLACES.find((e) => e.id === uid);
-  if (!userPlaces) {
-    const error = new Error("Could not find a place for the provided user id");
-    error.code = 404;
-    return next(error);
-  }
-  res.json({ userPlaces });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
