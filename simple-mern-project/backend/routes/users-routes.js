6e3ccac208fbf0d7be2d31207e14a509a26adf63
igordbauer <igordbauer@gmail.com>
2023-02-27 10:48:22 -0300

    hot-fix-integration-finished

--- simple-mern-project/backend/routes/users-routes.js
+++ simple-mern-project/backend/routes/users-routes.js
@@ -13,7 +13,7 @@ router.post(
   [
     check("name").notEmpty(),
     check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 5 }),
+    check("password").isLength({ min: 6 }),
   ],
   usersControllers.signUp
 );
--- simple-mern-project/frontend/src/user/pages/Auth.js
+++ simple-mern-project/frontend/src/user/pages/Auth.js
@@ -122,8 +122,8 @@ const Auth = () => {
             element="input"
             type="password"
             label="Password"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid passwrod (at least 5 characters)"
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText="Please enter a valid passwrod (at least 6 characters)"
             onInput={inputHandler}
           />
           <Button type="submit" disabled={!formState.isValid}>
